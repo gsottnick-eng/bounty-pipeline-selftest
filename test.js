04d731b2aa44ba0c86afe94e82b9b1e483d72ff3
Gavin Sottnick <gsottnick-eng@users.noreply.github.com>
2026-09-02 13:06:30 -0400

    fix: Add a subtract function alongside add
    Fixes #1

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import assert from 'node:assert';
-import {add} from './index.js';
-assert.strictEqual(add(2, 2), 4);
-console.log('1 test passed');
+import {add, subtract} from './index.js';
+assert.strictEqual(add(2, 4), 6);
+assert.strictEqual(subtract(5, 3), 2);
+console.log('2 tests passed');
